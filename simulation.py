897699a3de31bcb081dfe5e39a28f8a823f6adc2
atang-wonderfi <adrian.s.tang@gmail.com>
2024-06-20 21:33:52 -0400

    trivial save

--- simulation.py
+++ simulation.py
@@ -2,6 +2,7 @@ import numpy as np
 from scipy.integrate import solve_ivp
 import matplotlib.pyplot as plt
 import plotly.graph_objects as go
+from classes import *
 
 '''
 constants and initial variables
@@ -16,12 +17,18 @@ m2 = 2.71828
 m3 = 3.14159
 
 # initial positions (m)
-r1_initial = [0.0, 0.0, 0.0]
-r2_initial = [-1.0, 0.0, 0.0]
-r3_initial = [1.0, 0.0, 0.0]
+r1_initial = np.array([0.0, 0.0, 0.0])
+r2_initial = np.array([-1.0, 0.0, 0.0])
+r3_initial = np.array([1.0, 0.0, 0.0])
 
 # initial velocities (m/s)
-v1_initial = [0.0, 0.0, 0.0]
-v2_initial = [0.0, 1.0, 0.0]
-v3_initial = [0.0, -1.0, 0.0] 
+v1_initial = np.array([0.0, 0.0, 0.0])
+v2_initial = np.array([0.0, 1.0, 0.0])
+v3_initial = np.array([0.0, -1.0, 0.0])
 
+# object construction
+b1 = Body(m1, r1_initial, v3_initial)
+b2 = Body(m2, r2_initial, v3_initial)
+b3 = Body(m3, r3_initial, v3_initial)
+bodies = np.array([b1,b2,b3])
+system = System(bodies)
